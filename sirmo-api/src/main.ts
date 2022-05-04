58078d5824cb3d17eddbc07769045b219a667e2c
toundji <olatoundjiagbahoungba@gmail.com>
2022-05-04 13:31:05 +0100

    migration

--- sirmo-api/src/main.ts
+++ sirmo-api/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
                 .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("/api/documentation",app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
