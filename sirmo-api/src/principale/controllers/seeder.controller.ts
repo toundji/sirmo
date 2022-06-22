1db754c9cc46a513d6481698509d874867d6da4a
toundji <olatoundjiagbahoungba@gmail.com>
2022-06-22 10:17:06 +0100

    Seeder rendu publiv

--- sirmo-api/src/principale/controllers/seeder.controller.ts
+++ sirmo-api/src/principale/controllers/seeder.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/public-decore';
 import { SeederService } from './../services/seeder.service';
 
 @ApiTags("Seeders")
@@ -9,6 +10,7 @@ export class SeedController {
     constructor(private readonly seederService: SeederService) {}
 
 
+  @Public()
   @Get()
   async findOne() {
     return await this.seederService.seed();
