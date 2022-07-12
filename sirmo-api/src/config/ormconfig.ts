6dc6ee519b9c600e4ae0850d728c9c822d173861
toundji <olatoundjiagbahoungba@gmail.com>
2022-07-12 15:11:39 +0100

    .....

--- sirmo-api/src/config/ormconfig.ts
+++ sirmo-api/src/config/ormconfig.ts
@@ -5,10 +5,10 @@ import 'dotenv/config'
 
 const ormConfig: TypeOrmOptionsFactory|any = {
     type: "postgres" ?? process.env.DATABASE_SG,
-    // url: process.env.DB_URL,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // },
+    url: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
     host: process.env.DATABASE_HOST,
     port: (process.env.DATABASE_PORT as any) as number,
     username: process.env.DATABASE_USER,
