0849a39a32a107281a2f78c432b5b4dadf54dac9
toundji <olatoundjiagbahoungba@gmail.com>
2022-08-05 13:05:29 +0100

    ......

--- sirmo-api/src/enums/licence-property.ts
+++ sirmo-api/src/enums/licence-property.ts
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 export enum LicenceProperty {
     PRIX_LICENCE = 'PRIX LICENCE',
-    DUREE_DUREE = 'DUREE LICENCE'
+    DUREE_LICENCE = 'DUREE LICENCE',
+    DUREE_AMNADE = 'DUREE AMANDE'
+
   }
   
