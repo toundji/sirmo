1077608dcc6ddc1ebd4063b937f1f3bc4e0f74d2
toundji <olatoundjiagbahoungba@gmail.com>
2022-09-06 15:19:06 +0100

    ......

--- zemplus/lib/screens/home/home.screen.dart
+++ zemplus/lib/screens/home/home.screen.dart
@@ -4,22 +4,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sirmo/components/app-bar.screen.dart';
-import 'package:sirmo/components/user-drawer.dart';
 import 'package:sirmo/screens/potefeuille/pay_conducteur.screen.dart';
 import 'package:sirmo/screens/potefeuille/portefeuille.component.dart';
 import 'package:sirmo/screens/statistique-conducteur/evaluate-conduteur.screen.dart';
 import 'package:sirmo/services/compte.service.dart';
 import 'package:sirmo/services/conducteur.sevice.dart';
 import 'package:sirmo/utils/app-util.dart';
-import 'package:sirmo/utils/network-info.dart';
 
 import '../../components/action-card.dart';
 import '../../components/personal_alert.dart';
 import '../../models/compte.dart';
+import '../../models/notifcation_badge.dart';
 import '../../models/user.dart';
 import '../../services/user.service.dart';
 import '../conducteur/choice-driver.screen.dart';
 import '../statistique-conducteur/statistique-conducteur.screen.dart';
+
+import 'package:overlay_support/overlay_support.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:sirmo/models/push_notis.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key, this.debug = false}) : super(key: key);
@@ -32,10 +38,14 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   Compte? compte;
   User? user;
-  
+
   late final FirebaseMessaging _messaging;
   late int _totalNotifications;
   PushNotification? _notificationInfo;
@@ -44,13 +54,13 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     user = context.read<UserService>().user;
-    
+
     late int _totalNotifications;
     PushNotification? _notificationInfo;
 
     loadMyConducteurList();
-    
-     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       PushNotification notification = PushNotification(
         title: message.notification?.title,
         body: message.notification?.body,
@@ -68,6 +78,72 @@ class _HomeScreenState extends State<HomeScreen> {
         .onError((error, stackTrace) {
       PersonalAlert.showError(context, message: "êrror");
     });
+
+    checkForInitialMessage();
+    registerNotification();
+  }
+
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      PushNotification notification = PushNotification(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    }
+  }
+
+  void registerNotification() async {
+    // 1. Initialize the Firebase app
+    await Firebase.initializeApp();
+
+    // 2. Instantiate Firebase Messaging
+    _messaging = FirebaseMessaging.instance;
+
+    // 3. On iOS, this helps to take the user permissions
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+      });
+    } else {
+      print('User declined or has not accepted permission');
+    }
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   }
 
   loadMyConducteurList() {
@@ -145,7 +221,6 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
           PortefeuilleComponent(),
-          
         ],
       ),
     );
